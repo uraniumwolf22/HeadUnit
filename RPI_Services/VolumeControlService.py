1e7b6fe113430aee7a63a684ef2011b40c19e1e6
Logan Ross <logo3102@gmail.com>
2023-07-24 15:41:20 -0700

    Added basic volume control

---
+++ RPI_Services/VolumeControlService.py
@@ -0,0 +1,13 @@
+import serial
+import os
+
+def VolumeChange(dir,inc):
+        if dir: dir_="+"
+        else: dir_="-"
+        os.system(f"pactl set-sink-volume 0 {dir_}{inc}%")
+
+ser = serial.Serial('/dev/ttyS0', 115200)
+
+while True:
+        data=ser.readline()
+        VolumeChange(int(data.decode()[0]),1)
